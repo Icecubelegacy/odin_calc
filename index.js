4cbc77c144ca2cd74ef7310b86b20e29f916c77f
Andrew Choi <25736799+Icecubelegacy@users.noreply.github.com>
2024-06-15 22:12:59 +1000

    Complete calculator

--- index.js
+++ index.js
@@ -1,66 +1,156 @@
+//Constant enums
+const States = {
+  First: "firstStage",
+  Second: "secondStage"
+};
+
+const Operators = {
+  Add: "add",
+  Subtract: "subtract",
+  Multilpy: "multiply",
+  Divide: "divide",
+}
+
+//Global variables like state
 let firstNum = '';
 let operator = '';
 let secondNum = '';
-let opState = "firstStage";
+let opState = States.First;
+let prevOperator;
+let prevSecondNum = '';
+let rounding = 100000;
+
 
 function load() {
-  let btnNums = [...document.querySelectorAll('.num')];
-  btnNums.forEach((btn) => {
-    btn.addEventListener('click', clickNumber(btn.innerHTML));
+  let btns = [...document.querySelectorAll('button')];
+  btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      clickButton(btn)});
   });
-  let opNums = [...document.querySelectorAll('.operator')];
-  opNums.forEach((btn) => {
-    btn.addEventListener('click', clickOperator(btn.id));
-  });
-
 }
 
 function clickButton(btn) {
-  if (btn.class == "num") {
+  if (btn.className === "num") {
     clickNumber(btn.textContent);
+  }
+  else if (btn.className === "operator") {
+    clickOperator(btn.id);
+  }
+  else if (btn.className === "clearBtn") {
+    clickClear();
+  }
+  else if (btn.className === "equals") {
+    clickEquals();
+  }
+  else {
+    console.log(`Button ${btn.class} does not have a function!`);
+  }
+}
+
+function clickEquals()
+{
+  console.log("Enter clickEquals");
+  if (operator != '' && firstNum != '' && secondNum != '') {
+      operate();
+  }
+  else if (firstNum != '' && prevOperator != '' && prevSecondNum != '') {
+    operateAgain();
   }
 }
 
 function clickNumber(num) {
   console.log(`button ${num} clicked!`);
-  if (opState === "firstStage") {
+  if (opState === States.First) {
+    if (num === '.' && firstNum.includes('.')) return;
     firstNum = `${firstNum}${num}`;
   }
-  else if (opState === "secondStage") {
+  else if (opState === States.Second) {
+    if (num === '.' && secondNum.includes('.')) return;
     secondNum = `${secondNum}${num}`;
   }
-
   updateDisplay();
 }
 
 function clickOperator(op) {
   console.log(`button ${op} clicked!`);
-  if (opState === "firstStage") {
+  if (opState === States.First) {
     operator = op;
-    opState = "secondStage";
+    opState = States.Second;
+    console.log("Entered first state");
   }
-  else if (opState === "secondStage") {
-    
+  else if (opState === States.Second) {
+    console.log("Entered second state");
+    operate();
+    operator = op;
+    opState=States.Second
   }
 
+}
+
+function clickClear() {
+  console.log("entered clearAll");
+  resetState();
+  resetPrevState();
   updateDisplay();
 }
 
-function clearAll() {
+function operate(firstNumber = firstNum, secondNumber = secondNum, operater1 = operator) {
+  console.log("Enter operate");
+  console.log(`firstNumber=${firstNumber},operater1=${operater1},secondNumber=${secondNumber},opState=${opState}`);
+
+  const num1 = Number.parseFloat(firstNumber);
+  const num2 = Number.parseFloat(secondNumber);
+  let result;
+  switch (operater1) {
+    case Operators.Add:
+      result = num1 + num2;
+      break;
+    case Operators.Subtract:
+      result = num1 - num2;
+      break;
+    case Operators.Multilpy:
+      result = num1 * num2;
+      break;
+    case Operators.Divide:
+      if (num2 === 0) { 
+        console.log("To infinity and beyond...");
+      }
+      result = num1 / num2;
+      break;
+    default:
+      console.log(`Operator ${operater1} not implemennted!`);
+      break;
+  } 
+
+  prevSecondNum = secondNumber;
+  prevOperator = operater1;
+
+  resetState();
+  firstNum = result.toString() === 'Infinity' ? '' : Math.round(result * rounding) / rounding;
+  updateDisplay();
+}
+
+function operateAgain() {
+  console.log("Enter operateAgain");
+  operate(firstNum, prevSecondNum, prevOperator);
+}
+
+function updateDisplay() {
+  let display = document.querySelector(".display");
+  console.log(`firstNum=${firstNum},operator=${operator},secondNum=${secondNum},opState=${opState}`);
+  display.textContent = opState === States.First ? firstNum : secondNum;
+}
+
+function resetState() {
+  opState = States.First;
   firstNum = '';
   secondNum = '';
   operator = '';
-  updateDisplay();
 }
 
-function calculate() {
-  
-}
-
-
-function updateDisplay(btn) {
-  let display = document.querySelector(".display");
-  display.textContent = `${firstNum}`;
+function resetPrevState() {
+  prevSecondNum = '';
+  prevOperator = '';
 }
 
 load();
